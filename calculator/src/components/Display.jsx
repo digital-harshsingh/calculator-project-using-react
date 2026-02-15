b66562c342410641fcd1cf8e53908772b549bac8
Harsh Singh <digital.harshsingh@gmail.com>
2026-02-16 02:57:48 +0530

    Deleted node_modules folder

--- calculator/src/components/Display.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './Display.module.css';
-const Display = ({change}) => {
-  return  <input className={styles.display}type='text' value={change}/>
-   
-  
-}
-
-export default Display;
